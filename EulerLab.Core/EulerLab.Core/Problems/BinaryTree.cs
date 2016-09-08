f5aa7bb7ce404c3601aafa4e1de42a22b13ad242
pavel-yermalovich <x48ray@gmail.com>
2016-09-08 23:57:35 +0300

    First test

---
+++ EulerLab.Core/EulerLab.Core/Problems/BinaryTree.cs
@@ -0,0 +1,14 @@
+﻿namespace EulerLab.Core.Problems
+{
+    public class BinaryTree<T>
+    {
+        public BinaryTree(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+        public BinaryTree<T> Left { get; set; }
+        public BinaryTree<T> Right { get; set; } 
+    }
+}
---
+++ EulerLab.Core/EulerLab.Tests/Problem18Tests.cs
@@ -0,0 +1,22 @@
+﻿using EulerLab.Core.Problems;
+using NUnit.Framework;
+
+namespace EulerLab.Tests
+{
+    [TestFixture]
+    public class Problem18Tests
+    {
+        [Test]
+        public void WeCanCreateABinaryTree()
+        {
+            var tree = new BinaryTree<int>(1);
+            tree.Left = new BinaryTree<int>(2) { Left = new BinaryTree<int>(4), Right = new BinaryTree<int>(5) };
+            tree.Right = new BinaryTree<int>(3) { Left = new BinaryTree<int>(6), Right = new BinaryTree<int>(7) };
+
+            Assert.That(tree, Is.Not.Null);
+            Assert.That(tree.Value, Is.EqualTo(1));
+            Assert.That(tree.Left, Is.Not.Null);
+            Assert.That(tree.Right, Is.Not.Null);
+        }
+    }
+}
